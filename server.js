92b5883a0b5dd801506f2e27d7523079ae617e16
shubhmcoe <shubhmcoe@gmail.com>
2017-09-09 23:03:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req,res){
-  res.send('article one is being served here')});
+  res.send('article one is being served here');});
 
 app.get('/article-three',function (req,res){
-  res.send('article three is being served here')});
+  res.send('article three is being served here');});
 
 app.get('/article-two',function (req,res){
-  res.send('article two is being served here')});
+  res.send('article two is being served here');});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
